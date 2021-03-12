398909c797ce2ac50e385df16a5800e4c3b4abd4
Luis Alberto Zagonel Pozenato <luis.pozenato@cwi.com.br>
2021-03-12 15:09:10 -0300

    feat: sort and filter by reponame

--- README.md
+++ README.md
@@ -29,4 +29,6 @@ Run `rng --help` to see all available parameters.
 - `-p, --projectNumber`: The number of the project in which your cards are beeing shown. This number can be found at the URL of the project.
 - `-t, --token`: Your personal token. Don't forget to give `org` and `repo` admin permission.
 - `-l, --label`: Pass any valid label if you want to filter your return with one.
-- `-c, --column`: Pass the name of the colon that you want to use to generate the data, if you don't passa any, the first column of the project will be used.
+- `-c, --column`: Pass the name of the colon that you want to use to generate the data. This is required.
+- `-r, --repository`: Pass the name of the repository that you want to filter cards.
+- `-s, --sort`: Use to asc sort by card issue number.
--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ const yargs = require('yargs')
 const { hideBin } = require('yargs/helpers')
 const api = require('./services/api/index.js')
 
-const { organizationName, projectNumber, token, label, column } = yargs(
-  hideBin(process.argv)
-)
+const {
+  organizationName,
+  projectNumber,
+  token,
+  label,
+  column,
+  isSorted,
+  repository,
+} = yargs(hideBin(process.argv))
   .option('organizationName', {
     alias: 'o',
     type: 'string',
@@ -29,39 +35,90 @@ const { organizationName, projectNumber, token, label, column } = yargs(
     type: 'string',
     description: 'Define label to filter cards',
   })
+  .option('isSorted', {
+    alias: 's',
+    type: 'boolean',
+    description: 'Sort by issue number.',
+  })
   .option('column', {
     alias: 'c',
     type: 'string',
     description: 'Column name from where cards will be taken.',
+    demandOption: 'Please provide a valid Github project Column.',
+  })
+  .option('repository', {
+    alias: 'r',
+    type: 'string',
+    description: 'Filter cards by the associated repository.',
   }).argv
 
 const runtimeHeaders = {
   Authorization: `token ${token}`,
 }
 
+function sortCardsByIssueNumber(cardsInfo) {
+  return cardsInfo.sort((a, b) => {
+    if (a.number > b.number) {
+      return 1
+    }
+    if (a.number < b.number) {
+      return -1
+    }
+    return 0
+  })
+}
+
+function renderCards(cardsInfo) {
+  let cards = filterCards(cardsInfo)
+
+  if (isSorted) {
+    cards = sortCardsByIssueNumber(cards)
+  }
+
+  cards.forEach(card => {
+    renderCard(card)
+  })
+}
+
 function renderCard({ number, title }) {
   console.log(`#${number} ${title}`)
 }
 
-function getCardsInfo(cards) {
-  cards.forEach(async card => {
-    const { data: cardInfo } = await api.get(card.content_url, {
-      headers: runtimeHeaders,
-    })
-
-    if (!label) {
-      renderCard({ number: cardInfo.number, title: cardInfo.title })
-      return
+function filterCards(cards) {
+  return cards.filter(card => {
+    let preserveCard = true
+    if (repository) {
+      preserveCard = card.repository.toLowerCase() === repository.toLowerCase()
     }
-
-    const hasLabel = cardInfo.labels.some(
-      e => e.name.toLowerCase() === label.toLowerCase()
-    )
-
-    if (hasLabel) {
-      renderCard({ number: cardInfo.number, title: cardInfo.title })
+    if (label) {
+      preserveCard = card.labels.some(
+        e => e.name.toLowerCase() === label.toLowerCase()
+      )
     }
+    return preserveCard
   })
+}
+
+function getCardsInfo(cards) {
+  return Promise.all(
+    cards.map(async (card, i) => {
+      const { data: cardInfo } = await api.get(card.content_url, {
+        headers: runtimeHeaders,
+      })
+
+      const {
+        data: { name: repoName },
+      } = await api.get(cardInfo.repository_url, {
+        headers: runtimeHeaders,
+      })
+      return {
+        number: cardInfo.number,
+        title: cardInfo.title,
+        labels: cardInfo.labels,
+        repository: repoName,
+      }
+    })
+  )
 }
 
 async function loadReleaseNotes() {
@@ -71,8 +128,14 @@ async function loadReleaseNotes() {
       headers: runtimeHeaders,
     }
   )
+  if (!orgProjects) {
+    return
+  }
 
   const filteredProject = orgProjects.find(p => p.number === projectNumber)
+  if (!filteredProject) {
+    return
+  }
 
   const { data: columns } = await api.get(
     `https://api.github.com/projects/${filteredProject.id}/columns`,
@@ -80,16 +143,23 @@ async function loadReleaseNotes() {
       headers: runtimeHeaders,
     }
   )
+  if (!columns) {
+    return
+  }
 
   const filteredColumn = columns.find(col => col.name === column)
+  if (!filteredColumn) {
+    return
+  }
 
-  const { cards_url: cardsUrl } = column ? filteredColumn : columns[0]
-
+  const { cards_url: cardsUrl } = filteredColumn
   const { data: cards } = await api.get(`${cardsUrl}?per_page=100`, {
     headers: runtimeHeaders,
   })
 
-  getCardsInfo(cards)
+  getCardsInfo(cards).then(cardsInfo => {
+    renderCards(cardsInfo)
+  })
 }
 
 loadReleaseNotes()
